a16139ad05ac316d7d60d05c476968d75a345ed2
sunny <115807872+sunny4260@users.noreply.github.com>
2022-12-15 09:04:00 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /* int marks;
+     cin>>marks;
+      if(marks > 80){
+        cout<<"A"<<endl;
+      }
+      else if(marks > 60){
+        cout<<"B"<<endl;
+      }
+      else if(marks > 40){
+        cout<<"C"<<endl;
+      }
+      else{
+        cout<<"F"<<endl;
+      }*/
+
+     /* int num;
+      cin>>num;
+
+      if(num%2==0){
+        cout<<"even"<<endl;
+      }else{
+        cout<<"odd"<<endl;
+      }*/
+    
+   /* int age;
+    cin>>age;
+
+    if(age<12){
+        cout<<"child"<<endl;
+
+    }
+    else if(age>18){
+        cout<<"adult"<<endl;
+    }
+    else{
+        cout<<"teenager"<<endl;
+    }*/
+
+   /* int marks;
+    cin>>marks;
+    if(marks>33){
+      if(marks>83)
+      cout<<"outstanding";
+        else
+       cout<<"pass";
+    }else{
+      cout<<"fail"<<endl;
+    }*/
+
+    /* int marks;
+     cin>>marks;
+
+     marks>33 ? cout<<"pass" : cout<<"fail"<<endl;*/ 
+
+      /* int num;
+       cin>>num;
+       if((num%3==0) || (num%5==0)){
+        cout<<num<<endl;
+       } */
+      
+      int x=60,y=90;
+      if(y*x==540 ||  x+y==150){
+        cout<<"good\n";
+      }
+      else{
+        cout<<"bad\n";
+      }
+
+    return 0;
+}
