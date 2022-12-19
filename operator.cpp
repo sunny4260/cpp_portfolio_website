cf158fdbeb7460905ba190029806b56cebffc26f
sunny <115807872+sunny4260@users.noreply.github.com>
2022-12-19 09:30:50 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // arithmatic operator.
+
+   // int num1=5;
+    //int num2=8;
+
+   /* cout<<num1+num2<<endl;
+    cout<<num1-num2<<endl;
+    cout<<num1*num2<<endl;
+    cout<<num1/num2<<endl;
+    cout<<num1%num2<<endl; */
+
+    // Relational operator.
+
+   /* cout<<(num1==num2)<<endl;
+     cout<<(num1!=num2)<<endl;
+      cout<<(num1>=num2)<<endl;
+       cout<<(num1<=num2)<<endl;*/
+    
+    //logical operator.
+
+   /* bool exp1=true;
+    bool exp2=false;
+
+    cout<<(exp1&&exp2)<<endl;
+    cout<<(exp1||exp2)<<endl;
+    cout<<(! exp2)<<endl;*/
+
+    // assignment operator.
+
+   /* num1+=4;
+    cout<<num1<<endl;
+    num2-=1;
+    cout<<num2<<endl;
+    num1/=2;
+    cout<<num1<<endl;
+    num1%=3;
+    cout<<num1<<endl;*/
+
+    // Bitwise operator
+
+    /* cout<<(num1<<1)<<endl;
+    cout<<(num1>>1)<<endl;
+
+    cout<<(num1&num2)<<endl;
+    cout<<(num1|num2)<<endl;
+    cout<<(num1^num2)<<endl;*/
+
+    //misc operator.
+
+  /*  int a=4,name;
+    cout<<sizeof(a)<<endl;
+     
+     bool flag;
+    a == name? flag=true:flag=false;
+
+    cout<<flag<<endl;
+    cout<<(&a)<<endl; */
+
+    // unary operator.
+
+   /* int a=6;
+    int b=a+5;
+    
+    cout<<(a++)<<endl;
+    cout<<( b)<<endl;*/
+
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
