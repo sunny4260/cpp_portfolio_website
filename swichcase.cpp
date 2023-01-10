84a9a1776ee5ae1ff8d6b8985043e689ea781b20
sunny <115807872+sunny4260@users.noreply.github.com>
2023-01-10 19:53:02 +0530

    Add files via upload

---
+++ swichcase.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /* int day;
+     cin>>day;
+     switch (day)
+     {
+     case 1 : 
+        cout<<"Monday"<<endl;
+        break;
+     case 2 : 
+        cout<<"Tuesday"<<endl;
+        break; 
+     case 3 : 
+        cout<<"Wednesday"<<endl;
+        break;
+     case 4 : 
+        cout<<"Thursday"<<endl;
+        break;
+     case 5 : 
+        cout<<"Friday"<<endl;
+        break;   
+     case 6 : 
+        cout<<"Saturday"<<endl;
+        break;               
+     
+     default:
+        cout<<"Sunday"<<endl;
+        break;
+     }*/
+
+     char character;
+     cin>>character;
+     switch(character)
+     {
+        case 'a':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'e':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'i':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'o':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'u':
+            cout<<"Vowel"<<endl;
+            break;  
+        default:
+            cout<<"consonate"<<endl;                  
+     }
+
+     return 0;
+}
